f63895613c4f0e62d8aca594128403e5ce73ab03
AaronErhardt <AaronErhardt@users.noreply.github.com>
2026-05-14 15:13:39 +0000

    deploy: Relm4/book@60f50feb85590fc1e00f5ca64214c2ccadc22167

--- book/stable/searcher-c2a407aa.js
+++ book/stable/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-68957521.js',
+                path_to_root + 'searchindex-643ef981.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
